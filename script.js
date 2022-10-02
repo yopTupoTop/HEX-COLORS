aa052df25cbed6ad61dc4806978ad855cdddaedd
Tatiana <user@MacBook-Air-User.local>
2022-10-02 19:00:27 +0400

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+function changeColor(){
+    var hexNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+    
+    var hexcode = '';
+
+    for(var i = 0; i < 6; i++){
+        var randomIndex = Math.floor(Math.random() * hexNumbers.length); 
+
+        hexcode += hexNumbers[randomIndex]
+    }
+    document.getElementById("hex-code").innerHTML = hexcode;
+    document.getElementsByTagName("body")[0].style.background = "#" + hexcode;
+}
